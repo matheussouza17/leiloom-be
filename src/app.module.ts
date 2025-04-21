16ecdc5e2d08453e9b731694899b6ac756f7217b
matheus-henrique <matheus.henrique@askblue.com>
2025-04-21 13:18:44 -0300

    First Commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [UserModule, ConfigModule.forRoot({
+    isGlobal: true,
+  })], 
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Radar Leilão API')
+    .setDescription('Documentação da API do Radar Leilão')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3333);
+  console.log(`Message: ${process.env.Message}`);
+}
+
+bootstrap();
